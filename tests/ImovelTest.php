059f726e57287d3198d5ee73be0ce45e33e18546
Lucas Garcia Marcos <lucasgmarcos@gmail.com>
2023-10-06 13:47:04 -0300

    Testes unitários

--- tests/ImovelTest.php
+++ tests/ImovelTest.php
@@ -16,9 +16,75 @@ class ImovelTest extends TestCase
         $response = $this->json('POST', '/create', $dados);
 
         $response->seeStatusCode(201);
-
-        $response->seeJson([
-            'preco' => 120
-        ]);
     }
+
+    public function test_buscar_imovel()
+    {
+        $response = $this->json('GET', '/list');
+        $json = json_decode($response->response->getContent(), true);
+        $last = end($json);
+
+        $this->assertArrayHasKey('id', $last);
+
+        $id = $last['id'];
+
+        $response = $this->json('GET', '/get/' . $id);
+
+        $json = json_decode($response->response->getContent(), true);
+
+        $this->assertArrayHasKey('id', $json);
+    }
+
+    public function test_listar_imoveis()
+    {
+        $response = $this->json('GET', '/list');
+
+        $response->seeStatusCode(200);
+        $json = json_decode($response->response->getContent(), true);
+        $this->assertNotEmpty($json);
+    }
+
+    public function test_deletar_imovel()
+    {
+        $response = $this->json('GET', '/list');
+        $json = json_decode($response->response->getContent(), true);
+        $last = end($json);
+
+        $this->assertArrayHasKey('id', $last);
+
+        $id = $last['id'];
+
+        $response = $this->json('GET', '/delete/' . $id);
+
+        $response->seeStatusCode(200);
+    }
+
+    public function test_atualizar_imovel()
+    {
+        $dados = [
+            'endereco'  => 'Teste',
+            'preco'     => 120,
+            'tipo'      => 'A',
+            'status'    => 'A',
+        ];
+
+        $response = $this->json('POST', '/create', $dados);
+
+        $json = json_decode($response->response->getContent(), true);
+
+        $this->assertArrayHasKey('id', $json);
+
+        $dados = [
+            'id'        => $json['id'],
+            'endereco'  => 'Teste1',
+            'preco'     => 22.22,
+            'tipo'      => 'A',
+            'status'    => 'V',
+        ];
+
+        $response = $this->json('POST', '/create', $dados);
+
+        $response->seeStatusCode(200);
+    }
+    
 }
